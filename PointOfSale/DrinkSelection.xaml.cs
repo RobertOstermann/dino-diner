4eec2741343884bfe411be8d67557e11e3fe993c
RobertOstermann <loothunder@gmail.com>
2019-10-13 21:56:15 -0500

    Point of Sale Milestone 1 Complete.

--- PointOfSale/DrinkSelection.xaml.cs
+++ PointOfSale/DrinkSelection.xaml.cs
@@ -39,11 +39,9 @@ namespace PointOfSale
             SelectDecafButton.IsEnabled = false;
             SelectDecafButton.Visibility = Visibility.Hidden;
             SelectFlavorButton.IsEnabled = false;
-            SelectFlavorButton.Visibility = Visibility.Hidden;
+            SelectFlavorButton.Visibility = Visibility.Visible;
             WaterButton.BorderBrush = Brushes.LightGreen;
             WaterButton.BorderThickness = new Thickness(2.5);
-            //WaterButton.ClearValue(Control.BorderBrushProperty);
-            //WaterButton.ClearValue(Control.BorderThicknessProperty);
         }
 
         /// <summary>
@@ -65,6 +63,7 @@ namespace PointOfSale
             SelectDecafButton.Visibility = Visibility.Hidden;
             SelectFlavorButton.IsEnabled = true;
             SelectFlavorButton.Visibility = Visibility.Visible;
+            AddLemonButton.IsEnabled = false;
             SodasaurusButton.BorderBrush = Brushes.LightGreen;
             SodasaurusButton.BorderThickness = new Thickness(2.5);
 
@@ -74,6 +73,99 @@ namespace PointOfSale
             JurassicJavaButton.ClearValue(Control.BorderThicknessProperty);
             WaterButton.ClearValue(Control.BorderBrushProperty);
             WaterButton.ClearValue(Control.BorderThicknessProperty);
+        }
+
+        /// <summary>
+        /// Sets the drink to tyrannotea.
+        /// Enables the sweet button.
+        /// Enables the lemon button.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="args"></param>
+        private void SelectTyrannotea(object obj, RoutedEventArgs args)
+        {
+            Drink = new Tyrannotea();
+            SmallButton.Background = Brushes.LightGreen;
+            MediumButton.ClearValue(Control.BackgroundProperty);
+            LargeButton.ClearValue(Control.BackgroundProperty);
+            SelectSweetButton.IsEnabled = true;
+            SelectSweetButton.Visibility = Visibility.Visible;
+            SelectDecafButton.IsEnabled = false;
+            SelectDecafButton.Visibility = Visibility.Hidden;
+            SelectFlavorButton.IsEnabled = false;
+            SelectFlavorButton.Visibility = Visibility.Hidden;
+            AddLemonButton.IsEnabled = true;
+            TyrannoteaButton.BorderBrush = Brushes.LightGreen;
+            TyrannoteaButton.BorderThickness = new Thickness(2.5);
+
+            SodasaurusButton.ClearValue(Control.BorderBrushProperty);
+            SodasaurusButton.ClearValue(Control.BorderThicknessProperty);
+            JurassicJavaButton.ClearValue(Control.BorderBrushProperty);
+            JurassicJavaButton.ClearValue(Control.BorderThicknessProperty);
+            WaterButton.ClearValue(Control.BorderBrushProperty);
+            WaterButton.ClearValue(Control.BorderThicknessProperty);
+        }
+
+        /// <summary>
+        /// Sets the drink to Jurrasic Java.
+        /// Enables the Decaf button.
+        /// Disables the lemon button.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="args"></param>
+        private void SelectJurrasicJava(object obj, RoutedEventArgs args)
+        {
+            Drink = new JurassicJava();
+            SmallButton.Background = Brushes.LightGreen;
+            MediumButton.ClearValue(Control.BackgroundProperty);
+            LargeButton.ClearValue(Control.BackgroundProperty);
+            SelectSweetButton.IsEnabled = false;
+            SelectSweetButton.Visibility = Visibility.Hidden;
+            SelectDecafButton.IsEnabled = true;
+            SelectDecafButton.Visibility = Visibility.Visible;
+            SelectFlavorButton.IsEnabled = false;
+            SelectFlavorButton.Visibility = Visibility.Hidden;
+            AddLemonButton.IsEnabled = false;
+            JurassicJavaButton.BorderBrush = Brushes.LightGreen;
+            JurassicJavaButton.BorderThickness = new Thickness(2.5);
+
+            SodasaurusButton.ClearValue(Control.BorderBrushProperty);
+            SodasaurusButton.ClearValue(Control.BorderThicknessProperty);
+            TyrannoteaButton.ClearValue(Control.BorderBrushProperty);
+            TyrannoteaButton.ClearValue(Control.BorderThicknessProperty);
+            WaterButton.ClearValue(Control.BorderBrushProperty);
+            WaterButton.ClearValue(Control.BorderThicknessProperty);
+        }
+
+        /// <summary>
+        /// Sets the drink to Water.
+        /// Disables Decaf/Sweet/Flavor buttons.
+        /// Enables the lemon button.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="args"></param>
+        private void SelectWater(object obj, RoutedEventArgs args)
+        {
+            Drink = new Water();
+            SmallButton.Background = Brushes.LightGreen;
+            MediumButton.ClearValue(Control.BackgroundProperty);
+            LargeButton.ClearValue(Control.BackgroundProperty);
+            SelectSweetButton.IsEnabled = false;
+            SelectSweetButton.Visibility = Visibility.Hidden;
+            SelectDecafButton.IsEnabled = false;
+            SelectDecafButton.Visibility = Visibility.Hidden;
+            SelectFlavorButton.IsEnabled = false;
+            SelectFlavorButton.Visibility = Visibility.Visible;
+            AddLemonButton.IsEnabled = true;
+            WaterButton.BorderBrush = Brushes.LightGreen;
+            WaterButton.BorderThickness = new Thickness(2.5);
+
+            SodasaurusButton.ClearValue(Control.BorderBrushProperty);
+            SodasaurusButton.ClearValue(Control.BorderThicknessProperty);
+            TyrannoteaButton.ClearValue(Control.BorderBrushProperty);
+            TyrannoteaButton.ClearValue(Control.BorderThicknessProperty);
+            JurassicJavaButton.ClearValue(Control.BorderBrushProperty);
+            JurassicJavaButton.ClearValue(Control.BorderThicknessProperty);
         }
 
         /// <summary>
--- PointOfSale/FlavorSelection.xaml.cs
+++ PointOfSale/FlavorSelection.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/* FlavorSelection.xaml.cs
+ * Author: Robert Ostermann
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
