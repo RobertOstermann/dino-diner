ab15bd5ad0559fea7ed894f8261ef7f60042ee2f
RobertOstermann <loothunder@gmail.com>
2019-11-21 14:00:16 -0600

    Category Search.

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -14,14 +14,65 @@ namespace Website.Pages
 
         public List<CretaceousCombo> Combos;
 
+        public bool ShowCombos
+        {
+            get
+            {
+                if (Combos != null)
+                {
+                    if (Combos.Count > 0) return true;
+                }
+                return false;
+            }
+        }
+
         public List<Entree> Entrees;
+
+        public bool ShowEntrees
+        {
+            get
+            {
+                if (Entrees != null)
+                {
+                    if (Entrees.Count > 0) return true;
+                }
+                return false;
+            }
+        }
 
         public List<Drink> Drinks;
 
+        public bool ShowDrinks
+        {
+            get
+            {
+                if (Drinks != null)
+                {
+                    if (Drinks.Count > 0) return true;
+                }
+                return false;
+            }
+        }
+
         public List<Side> Sides;
+
+        public bool ShowSides
+        {
+            get
+            {
+                if (Sides != null)
+                {
+                    if (Sides.Count > 0) return true;
+                }
+                return false;
+            }
+        }
 
         [BindProperty]
         public string search { get; set; }
+
+        [BindProperty]
+        public List<string> category { get; set; } = new List<string>() { "Combos", "Entrees", "Drinks", "Sides" };
 
         public void OnGet()
         {
@@ -34,17 +85,21 @@ namespace Website.Pages
         {
             if (search != null)
             {
-                Combos = menu.SearchCombos(search);
-                Entrees = menu.SearchEntrees(search);
-                Drinks = menu.SearchDrinks(search);
-                Sides = menu.SearchSides(search);
+                Combos = null;
+                Entrees = null;
+                Drinks = null;
+                Sides = null;
+                if (category.Contains("Combos")) Combos = menu.SearchCombos(search);
+                if (category.Contains("Entrees")) Entrees = menu.SearchEntrees(search);
+                if (category.Contains("Drinks")) Drinks = menu.SearchDrinks(search);
+                if (category.Contains("Sides")) Sides = menu.SearchSides(search);
             }
             else
             {
-                Combos = menu.AvailableCombos;
-                Entrees = menu.AvailableEntrees;
-                Drinks = menu.AvailableDrinks;
-                Sides = menu.AvailableSides;
+                if (category.Contains("Combos")) Combos = menu.AvailableCombos;
+                if (category.Contains("Entrees")) Entrees = menu.AvailableEntrees;
+                if (category.Contains("Drinks")) Drinks = menu.AvailableDrinks;
+                if (category.Contains("Sides")) Sides = menu.AvailableSides;
             }
         }
     }
